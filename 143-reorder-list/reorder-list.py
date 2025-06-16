7bc859f04e3e06a561b42f5c06a198dd72d0bea4
Musab131719 <musabrizwan@gmail.com>
2025-06-16 14:18:18 +0100

    Time: 9 ms (34.86%) | Memory: 30.8 MB (5.05%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,25 +5,31 @@
 #         self.next = next
 class Solution(object):
     def reorderList(self, head):
+
+        # Creating array with elements of linked list
         dummy = head
         A = []
         while dummy is not None:
             A.append(dummy.val)
             dummy = dummy.next
- 
+
+        # Creating a new array with sorted elements
         n = len(A)
         B = [] 
         for i in range(n/2):
             B.append(A[i])
             B.append(A[n-i-1])
         
+        # Adjusting for odd midpoint
         if n % 2 is not 0:
             B.append(A[n/2])
 
+        # Changing linked list to match the sorted array
         count = 0
         dummy_2 = head
         while count < n:
             head.val = B[count]
             count += 1
             head = head.next
+            
         return dummy_2
