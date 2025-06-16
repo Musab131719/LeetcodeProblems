7eede9a212b638eb2eb6d513d7c35b9c1efab9a1
Musab131719 <musabrizwan@gmail.com>
2025-06-16 13:15:10 +0100

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.37%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -5,6 +5,8 @@
 #         self.next = next
 class Solution(object):
     def removeNthFromEnd(self, head, n):
+        
+        # Finding the rth element from the head that we need to remove
         length = 0
         curr = head
         while curr is not None:
@@ -12,17 +14,19 @@ class Solution(object):
             curr = curr.next
         r = length - n + 1
         
+        # Handling if head of linked list needs to be removed
         if r == 1:
             head = head.next
             return head
 
+        # Traversing to the (r-1)th element  
         count = 1
         dummy = head
-
         while count < r - 1:
             head = head.next
             count += 1
 
+        # If last element needs to be removed, end list. Else, skip the following element 
         if length == r:
             head.next = None
         elif r + 1 <= length:       
