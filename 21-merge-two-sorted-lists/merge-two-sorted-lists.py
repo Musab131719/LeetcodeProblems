4e0aac968b28ec4d45b26d5c42b7c19e9acba4b2
Musab131719 <musabrizwan@gmail.com>
2025-06-16 12:33:34 +0100

    Time: 4 ms (13.01%) | Memory: 12.5 MB (73.92%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -7,18 +7,21 @@ class Solution(object):
     def mergeTwoLists(self, list1, list2):
         curr1, curr2 = list1, list2
         
+        # Checking emptyness
         if curr1 is None:
             return curr2
         if curr2 is None:
             return curr1
 
+        # Initialising head/first value of first node
         list3 = ListNode(min(curr1.val, curr2.val))
-        head = list3
+        head = list3                                    # Remember the start of list3 so we can return it
         if curr1.val <= curr2.val:
             curr1 = curr1.next
         else:
             curr2 = curr2.next
-
+        
+        # Adding values until one of the nodes from list1 and list2 are 'None'
         while curr1 is not None and curr2 is not None:
             if curr1.val <= curr2.val:
                 list3.next = ListNode(curr1.val)
@@ -28,11 +31,14 @@ class Solution(object):
                 curr2 = curr2.next
             list3 = list3.next
         
+        # Remaining values
         curr_rem = curr1 if curr1 is not None else curr2
 
+        # Adding remaining values
         while curr_rem is not None:
             list3.next = ListNode(curr_rem.val)
             list3 = list3.next
             curr_rem = curr_rem.next
+            
         return head
         
