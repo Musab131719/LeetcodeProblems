a4fa009f6ba3cb6764823cdc425f449f669fca2e
Musab131719 <musabrizwan@gmail.com>
2025-06-16 13:59:51 +0100

    Time: 90 ms (33.12%) | Memory: 90.7 MB (58.11%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.py
@@ -6,22 +6,27 @@
 import math
 class Solution(object):
     def deleteMiddle(self, head):
+        
+        # Handling list with 1 node
         if head.next == None:
             return None
 
+        # Calculating length and then finding rth element we need to remove 
         length = 0
         curr = head
         while curr is not None:
             length += 1
             curr = curr.next
-        
         r = int(math.ceil(length / 2.0)) if length % 2 is not 0 else 1 + length / 2
+        
+        # Traversing to the (r - 1)th element  
         count = 1 
         dummy = head
         while count < r - 1:
             count += 1
             head = head.next
 
+        # If r is last element, set to None, else skip 
         if length == r:
             head.next = None
         else:       
