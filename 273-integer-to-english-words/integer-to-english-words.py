11449534af52b127dbf813752e4a137aae0fda0c
Musab131719 <musabrizwan@gmail.com>
2025-06-17 15:25:04 +0100

    Time: 0 ms (100.00%) | Memory: 12.3 MB (83.65%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -3,17 +3,19 @@ class Solution(object):
         if num == 0:
             return "Zero"    
         
+        # Setting lookup dictionaries
         units = {"0":"", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"}
         tens = {"0":"", "1": "Ten","2": "Twenty", "3": "Thirty", "4": "Forty", "5": "Fifty", "6": "Sixty", "7": "Seventy", "8": "Eighty", "9": "Ninety"}
         teens = {"10":"Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen", "14": "Fourteen", "15": "Fifteen", "16": "Sixteen", "17": "Seventeen", "18": "Eighteen", "19": "Nineteen"}
         suffix = {1:"", 2:"Thousand", 3:"Million", 4:"Billion"}
-
+        
         num_list = [x for x in str(num)]
         original_reverse = str(num)[::-1]
         reverse = num_list[::-1]
         n = len(num_list)
         result = ""
 
+        # Converting digits into word form
         for i in range(n):
             digit = reverse[i]
             if i % 3 == 0:
@@ -31,18 +33,22 @@ class Solution(object):
                 else:
                     reverse[i] = tens[digit]
         
+        # Grouping first elements s.t. remaining elements mod 3 == 0
         flip = reverse[::-1]
         first_group = n % 3 or 3
         groups = []
         groups.append(" ".join([x for x in flip[:first_group] if x != ""]).strip())
         
+        # Grouping remaining elements in 3's
         for i in range(first_group, n, 3):
             groups.append(" ".join([x for x in flip[i:i+3] if x != ""]).strip())
 
+        # Adding suffixes to the groups like 'Thousand', 'Million' etc.
         for i in range(len(groups)): 
             if groups[i] is not "":   
                 groups[i] = groups[i] + " " + suffix[len(groups) - i]
 
+        # Combining all groups
         for group in groups:
             if group is not "":
                 result += " " + group
