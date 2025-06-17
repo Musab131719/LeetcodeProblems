1c2227e666e04b914a8e073369ee483bebd34d61
Musab131719 <musabrizwan@gmail.com>
2025-06-17 15:19:51 +0100

    Time: 2 ms (41.35%) | Memory: 12.6 MB (16.03%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -2,15 +2,12 @@ class Solution(object):
     def numberToWords(self, num):
         if num == 0:
             return "Zero"    
-
+        
         units = {"0":"", "1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"}
         tens = {"0":"", "1": "Ten","2": "Twenty", "3": "Thirty", "4": "Forty", "5": "Fifty", "6": "Sixty", "7": "Seventy", "8": "Eighty", "9": "Ninety"}
         teens = {"10":"Ten", "11": "Eleven", "12": "Twelve", "13": "Thirteen", "14": "Fourteen", "15": "Fifteen", "16": "Sixteen", "17": "Seventeen", "18": "Eighteen", "19": "Nineteen"}
         suffix = {1:"", 2:"Thousand", 3:"Million", 4:"Billion"}
 
-        if str(num) in units:
-            return units[str(num)]
-        
         num_list = [x for x in str(num)]
         original_reverse = str(num)[::-1]
         reverse = num_list[::-1]
