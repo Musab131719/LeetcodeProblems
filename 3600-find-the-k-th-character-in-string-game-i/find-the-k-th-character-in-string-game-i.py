2f8f35fea85a6b5ca72288254398fa403f202ccb
Musab131719 <musabrizwan@gmail.com>
2025-07-04 22:31:00 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (58.92%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -1,10 +1,10 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        count = 0
+        shift_count = 0
         while k != 1:
             n = ceil(math.log(k, 2)) - 1
             p = 2 ** n
             k -= p
-            count += 1
+            shift_count += 1
         # 97 is unicode for 'a'
-        return chr(97 + count)
+        return chr(97 + shift_count)
